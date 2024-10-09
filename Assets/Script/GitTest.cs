d48467eccee4e8f04e394dd24ca7f38b473765e3
tang_cong <1557258053@qq.com>
2024-10-09 11:16:19 +0800

    第一次提交

---
+++ Assets/Script/GitTest.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GitTest : MonoBehaviour
+{
+    private string content = "nmsl";
+    void Start()
+    {
+        Debug.Log(content);
+    }
+    void Update()
+    {
+        
+    }
+}
